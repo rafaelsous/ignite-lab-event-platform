16f7789fc9d2e1c589bf52771452119dc4071a48
Rafael Sousa <rafaelsousa.souza@gmail.com>
2022-06-22 23:02:57 -0300

    Commit inicial.

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client"
+
+type Lesson = {
+  id: string;
+  title: string;
+}
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  
+  return (
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
+}
+
+export default App
