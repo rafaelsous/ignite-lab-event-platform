da56c4610b022fd7005e1452baaedbf35a369192
Rafael Sousa <rafaelsousa.souza@gmail.com>
2022-06-25 15:39:35 -0300

    Implementação da funcionalidade de inscrição via graphql concluída.

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { format, isPast } from 'date-fns'
 import { ptBR } from 'date-fns/locale';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classnames from 'classnames'
 
 interface LessonProps {
   title: string;
@@ -11,40 +12,81 @@ interface LessonProps {
 }
 
 export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const { slug: currentSlug } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR
   });
 
+  const isActiveLesson = currentSlug === slug;
+
   return (
-    <Link to={`/event/lesson/${slug}`} className="group">
-      <span className="text-gray-300">
-        {availableDateFormatted}
-      </span>
+    <div className={classnames(
+        {
+          'cursor-not-allowed': !isLessonAvailable,
+        }
+      )}
+    >
+      <Link to={`/event/lesson/${slug}`} className={classnames('group', {
+        'pointer-events-none': !isLessonAvailable,
+      })}>
+        <span className="text-gray-300">
+          {availableDateFormatted}
+        </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
-        <header className="flex items-center justify-between">
-          {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-              <CheckCircle size={20} />
-              Conteúdo liberado
-            </span>
-          ) : (
-            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-              <Lock size={20} />
-              Em breve
-            </span>
+        <div
+          className={classnames(
+            'p-4 mt-2 rounded border',
+            {
+              'border-gray-500': !isActiveLesson,
+              'group-hover:border-green-500 transition-colors': isLessonAvailable && !isActiveLesson,
+              'bg-green-500 border-green-500': isLessonAvailable && isActiveLesson
+            }
           )}
-          
-          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-            {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-          </span>
-        </header>
+        >
+          <header className="flex items-center justify-between">
+            {isLessonAvailable ? (
+              <span className={classnames(
+                'text-sm text-blue-500 font-medium flex items-center gap-2',
+                {
+                  'text-white': isActiveLesson
+                }
+              )}
+            >
+                <CheckCircle size={20} />
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                <Lock size={20} />
+                Em breve
+              </span>
+            )}
+            
+            <span className={classnames(
+                'text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold',
+                {
+                  'border-white': isActiveLesson
+                }
+              )}
+            >
+              {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+            </span>
+          </header>
 
-        <strong className="text-gray-200 mt-5 block">
-          {title}
-        </strong>
-      </div>
-    </Link>
+          <strong className={classnames(
+              'mt-5 block',
+              {
+                'text-white': isActiveLesson,
+                'text-gray-200': !isActiveLesson
+              }
+            )}
+          >
+            {title}
+          </strong>
+        </div>
+      </Link>
+    </div>
   )
 }
